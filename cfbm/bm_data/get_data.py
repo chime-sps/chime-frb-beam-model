3e9c087b1d45bd0c44a538b52f080b4fc8ab15ef
robert.main@mcgill.ca <robert.main@mcgill.ca>
2025-11-17 11:13:07 -0500

    get data from github release

--- cfbm/bm_data/get_data.py
+++ cfbm/bm_data/get_data.py
@@ -36,18 +36,27 @@ def save_response_content(response, destination):
 
 def main():
     print("Downloading CHIME/FRB Beam Model")
+    # Download from GitHub Release assets (keeps files out of git repo to avoid bloat)
+    # Release version matches beam model data version (indexed by date)
+    RELEASE_TAG = "2022.5.1"
     file_ids = {
-        "beam_XX_v1.h5": "https://ws.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/files/vault/AstroDataCitationDOI/CISTI.CANFAR/22.0005/data/beam_XX_v1.h5",
-        "beam_YY_v1.h5": "https://ws.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/files/vault/AstroDataCitationDOI/CISTI.CANFAR/22.0005/data/beam_YY_v1.h5",
+        "beam_XX_v1.h5": f"https://github.com/chime-sps/chime-frb-beam-model/releases/download/{RELEASE_TAG}/beam_XX_v1.h5",
+        "beam_YY_v1.h5": f"https://github.com/chime-sps/chime-frb-beam-model/releases/download/{RELEASE_TAG}/beam_YY_v1.h5",
     }
+
+    directory = here + "/bm_data/"
+    if not os.path.exists(directory):
+        print("Making beam model data directory at {}...".format(directory))
+        os.makedirs(directory)
+
     for filename in file_ids.keys():
         print("Fetching: {}".format(filename))
-        directory = here + "/bm_data/"
-        if not os.path.exists(directory):
-            print("Making beam model data directory at {}...".format(directory))
         destination = directory + "{}".format(filename)
         if not os.path.isfile(destination):
             download_file_from_web(file_ids[filename], destination)
+        else:
+            print(f"{filename} already exists, skipping download")
+
     print("Download Complete")
 
 
